8db6c59d3ae676db5e90181ec49f49ae1b96335b
gabrielSantos1101 <gabriel.adv45@outlook.com>
2026-02-16 13:43:06 -0300

    build: update rollup config for ESM and better CLI bundling

--- src/services/update.js
+++ src/services/update.js
@@ -3,12 +3,9 @@ import path from "path";
 import os from "os";
 import ora from "ora";
 import inquirer from "inquirer";
-import { createRequire } from "module";
 import { UPDATE_CHECK_INTERVAL } from "../constants.js";
 import { isVersionOlder, executeCommand } from "../utils/helpers.js";
-
-const require = createRequire(import.meta.url);
-const packageJson = require("../../package.json");
+import packageJson from "../../package.json" assert { type: "json" };
 
 const fetch = globalThis.fetch;
 
